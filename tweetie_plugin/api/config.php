0f38be227bbea7ce74f66ca9f7cbf3d1024b1cdc
mrtrix64 <anneemanuel@Annes-MacBook-Pro.local>
2014-08-11 21:30:59 +0100

    trying twitter feed

---
+++ tweetie_plugin/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'DXmnpRB2J4XBjMIcRZ0MktdPK');
+    define('CONSUMER_SECRET', 'xTzkQuOL1iALfCqNpUNVWnZImYPeSKwV4aQYfS1p1jOpVAF2U3');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '15366492-V66ZLl9xkUIgFEVWOE8d7MidMkXy3idlobUXFkvVJ');
+    define('ACCESS_SECRET', 'wFqMWzPSTbGMiqa7VMV5UkN9IcGsZ4MUYdTfF6wurrHPs');
