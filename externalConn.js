3611c200618f6cc9fee88d51c6bd15c59eb6c312
mrtrix64 <mrtrix64@users.noreply.github.com>
2014-08-20 14:10:23 +0100

    Update externalConn.js

--- externalConn.js
+++ externalConn.js
@@ -28,4 +28,4 @@ var dsDates = new Spry.Data.XMLDataSet("http://e-update.co.uk/ttc_ios/data/dates
 dsDates.setColumnType("schedule_title_id", "number");
 dsDates.setColumnType("schedule_date", "date");
 
-var delegates = new Spry.Data.XMLDataSet("http://e-update.co.uk/ttc_ios/data/delegates.php?view_xml=1", "recordset/record");
+var delegates = new Spry.Data.XMLDataSet("http://e-update.co.uk/ttc_ios/data/delegates.xml", "recordset/record");
