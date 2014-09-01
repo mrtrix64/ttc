9b5fbf5271799beb6a5eba1b416717e6d064ff11
Colin Miller <post@colinmiller.co.uk>
2014-09-01 17:16:19 +0100

    Added mobile connections

---
+++ mobileConn.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+var dsSponsors = new Spry.Data.XMLDataSet("/data/sponsors.php?view_xml=1?view_xml=1", "recordset/record", {sortOrderOnLoad: "ascending"});
+dsSponsors.setColumnType("description", "html");
+dsSponsors.setColumnType("aims", "html");
+dsSponsors.setColumnType("website", "html");
+	var dsSchedule = new Spry.Data.XMLDataSet("/data/schedule.php?view_xml=1", "recordset/record", {sortOnLoad: "sort_order", sortOrderOnLoad: "ascending"});
+dsSchedule.setColumnType("title", "html");
+dsSchedule.setColumnType("sort_order", "number");
+	var dsHotels = new Spry.Data.XMLDataSet("/data/hotels.php?view_xml=1", "recordset/record", {sortOnLoad: "hotel_name", sortOrderOnLoad: "ascending"});
+dsHotels.setColumnType("hotel_name", "html");
+dsHotels.setColumnType("google_map", "html");
+dsHotels.setColumnType("description", "html");
+dsHotels.setColumnType("amenities", "html");
+dsHotels.setColumnType("website", "html");
+var dsSpecialist = new Spry.Data.XMLDataSet("/data/specialist.php?view_xml=1", "recordset/record");
+dsSpecialist.setColumnType("cms_content", "html");
+dsSpecialist.setColumnType("sort_order", "number");
+dsSpecialist.setColumnType("spkr_content", "html");
+var dsPlenary = new Spry.Data.XMLDataSet("/data/plenary.php?view_xml=1", "recordset/record");
+dsPlenary.setColumnType("cms_content", "html");
+dsPlenary.setColumnType("sort_order", "number");
+dsPlenary.setColumnType("spkr_content", "html");
+var dsForum = new Spry.Data.XMLDataSet("/data/marketing.php?view_xml=1", "recordset/record");
+dsForum.setColumnType("cms_content", "html");
+dsForum.setColumnType("sort_order", "number");
+dsForum.setColumnType("spkr_content", "html");
+var dsDates = new Spry.Data.XMLDataSet("/data/dates.php?view_xml=1", "recordset/record", {sortOnLoad: "schedule_title_id", sortOrderOnLoad: "ascending"});
+dsDates.setColumnType("schedule_title_id", "number");
+dsDates.setColumnType("schedule_date", "date");
+
+var delegates = new Spry.Data.XMLDataSet("/data/delegates.php", "recordset/records");
