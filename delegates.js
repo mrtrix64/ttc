414381ef01477da57da927b33039b96cc018e90f
Colin Miller <post@colinmiller.co.uk>
2014-09-08 16:22:17 +0100

    Got it! I think

--- delegates.js
+++ delegates.js
@@ -124,7 +124,7 @@ function checkConnection() {
     states[Connection.CELL_4G]  = 'Cell 4G connection';
     states[Connection.CELL]     = 'Cell generic connection';
     states[Connection.NONE]     = 'No network connection';
-	    alert('Connection type: ' + states[networkState]);
+	    //alert('Connection type: ' + states[networkState]);
 
 		if ((states[networkState]) == states[Connection.NONE]) {
 		$.mobile.changePage($("#offline"), "none");
