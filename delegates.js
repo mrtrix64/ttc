8f480222c9c93198514b4a486fcea5e82d3923a0
Colin Miller <post@colinmiller.co.uk>
2014-09-08 15:05:39 +0100

    Again

--- delegates.js
+++ delegates.js
@@ -117,23 +117,20 @@ function saveSettings() {
     function onDeviceReady() {
         // Now safe to use the PhoneGap API
 alert ("DEVICE IS READY");
-function checkConnection() {
-    var networkState = navigator.connection.type;
+var networkState = navigator.network.connection.type;
+var states = {};
+states[Connection.UNKNOWN]  = 'Unknown connection';
+states[Connection.ETHERNET] = 'Ethernet connection';
+states[Connection.WIFI]     = 'WiFi connection';
+states[Connection.CELL_2G]  = 'Cell 2G connection';
+states[Connection.CELL_3G]  = 'Cell 3G connection';
+states[Connection.CELL_4G]  = 'Cell 4G connection';
+states[Connection.NONE]     = 'No network connection';
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
-
-    alert('Connection type: ' + states[networkState]);
+if ((states[networkState]) == states[Connection.NONE])
+{
+alert("Please check your internet connectivity and try again"); 
 }
-
-checkConnection();
 
 }
 
