909614875e385e3abf0d9e5408f874636b60ab2c
Colin Miller <post@colinmiller.co.uk>
2014-09-08 14:08:00 +0100

    Desperate

--- delegates.js
+++ delegates.js
@@ -118,18 +118,29 @@ function saveSettings() {
         // Now safe to use the PhoneGap API
 alert ("DEVICE IS READY");
 
-function checkConnection() {
-        var networkState = navigator.network.connection.type;
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-        alert('Connection type: ' + states[networkState]);
-    }
+function init() {
+document.addEventListener("online", toggleCon, false);
+document.addEventListener("offline", toggleCon, false);
+ 
+if(navigator.network.connection.type == Connection.NONE) {
+navigator.notification.alert("Sorry, you are offline.", function() {}, "Offline!");
+} else {
+setupButtonHandler();
+}
+ 
+}
+ 
+function toggleCon(e) {
+console.log("Called",e.type);
+if(e.type == "offline") {
+$("#searchBtn").off("touchstart").attr("disabled","disabled");
+navigator.notification.alert("Sorry, you are offline.", function() {}, "Offline!");
+} else {
+$("#searchBtn").removeAttr("disabled");
+navigator.notification.alert("Woot, you are back online.", function() {}, "Online!");
+setupButtonHandler();
+}
+}
 
 
 }
