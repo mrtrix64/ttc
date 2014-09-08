f4559cc8376ed948e8a2f286354299223475ffda
Colin Miller <post@colinmiller.co.uk>
2014-09-08 13:07:58 +0100

    Delegates rewrite

---
+++ check_internet_connection.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+    var file = "http://www.yoursite.com/somefile.png";
+    var randomNum = Math.round(Math.random() * 10000);
+     
+    xhr.open('HEAD', file + "?rand=" + randomNum, false);
+     
+    try {
+        xhr.send();
+         
+        if (xhr.status >= 200 && xhr.status < 304) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
