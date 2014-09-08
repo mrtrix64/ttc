814250fb8db01680fc993a2ef4cfcca03bf6cdff
Colin Miller <post@colinmiller.co.uk>
2014-09-08 15:34:16 +0100

    x

--- delegates.js
+++ delegates.js
@@ -104,30 +104,27 @@ function saveSettings() {
 });
 
 
-// Wait for PhoneGap to load
-    // 
-    document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
+// device APIs are available
+//
 
-    // PhoneGap is loaded and it is now safe to make calls PhoneGap methods
-    //
-    function onDeviceReady() {
-        checkConnection();
+function onDeviceReady() {
+    isOnline();
+}
+
+function isOnline() {
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+    if ((states[networkState]) == states[Connection.NONE]) {
+        alert('No Internet Connection. Click OK to exit app');
+        navigator.app.exitApp();
     }
-
-    function checkConnection() {
-        var networkState = navigator.network.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        alert('Connection type: ' + states[networkState]);
-    }
-
-
-
+}
