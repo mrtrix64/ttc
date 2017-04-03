1d2902c779d628b3be1e582f8dfa0019f561da58
mrtrix64 <anneemanuel@Annes-MacBook-Pro.local>
2017-04-03 14:49:58 +0100

    updated config for new app

--- Connections/abtaFrontConn.php
+++ Connections/abtaFrontConn.php
@@ -2,15 +2,15 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_abtaFrontConn = "localhost";
-$database_abtaFrontConn = "db99712_abtatravel16";
-$username_abtaFrontConn = "db99712_anne";
-$password_abtaFrontConn = "treestump2010";
-$abtaFrontConn = mysql_connect($hostname_abtaFrontConn, $username_abtaFrontConn, $password_abtaFrontConn) or trigger_error(mysql_error(),E_USER_ERROR);
- 
 /*$hostname_abtaFrontConn = "internal-db.s99712.gridserver.com";
 $database_abtaFrontConn = "db99712_abtatravel16";
-$username_abtaFrontConn = "db99712_anne";
-$password_abtaFrontConn = "treestump2010";
+$username_abtaFrontConn = "db99712_user";
+$password_abtaFrontConn = "Treestump17@@";
 $abtaFrontConn = mysql_connect($hostname_abtaFrontConn, $username_abtaFrontConn, $password_abtaFrontConn) or trigger_error(mysql_error(),E_USER_ERROR); */
+
+$hostname_abtaFrontConn = "localhost";
+$database_abtaFrontConn = "db99712_abtatravel17";
+$username_abtaFrontConn = "db99712_user";
+$password_abtaFrontConn = "treestump2010";
+$abtaFrontConn = mysql_connect($hostname_abtaFrontConn, $username_abtaFrontConn, $password_abtaFrontConn) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
